6f69610dc66389493aa359d3510a6b81672bb6f2
priya-2645 <77579335+priya-2645@users.noreply.github.com>
2021-05-27 10:53:12 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,17 +5,13 @@ const db = new Sequelize('customer', 'root', 'Sairam3@', {
   host: 'localhost',
   dialect: 'mysql'
 });
-
-
 const User = db.define('User', {
-  // Model attributes are defined here
   firstName: {
     type: DataTypes.STRING,
     allowNull: false
   },
   lastName: {
     type: DataTypes.STRING
-    // allowNull defaults to true
   },
   address: {
     type: DataTypes.STRING
@@ -24,40 +20,17 @@ const User = db.define('User', {
     type: DataTypes.INTEGER
   },
 
-}, {
-  // Other model options go here
-});
+},
+);
 
-console.log(User === sequelize.models.User); // true
 const User1 = db.define('User1', {
-  // Model attributes are defined here
   firstName: {
     type: DataTypes.STRING,
     allowNull: false
   },
-  lastName: {
-    type: DataTypes.STRING
-    // allowNull defaults to true
-  },
-  address: {
-    type: DataTypes.STRING
-  },
-  age: {
-    type: DataTypes.INTEGER
-  },
 
-}, {
-  // Other model options go here
+
 });
-
-// `sequelize.define` also returns the model
-console.log(User1 === sequelize.models.User1); // true
-
-
-// db.authenticate()
-//   .then(() => {
-//     console.log('connection success')
-//   })
-module.exports - {
-  db
-}
+User.hasOne(User, { foreignKey: "firstName" });
+User1.belongsTo(User1);
+db.sync()
